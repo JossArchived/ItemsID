ded73b9f8a04f21c62b4c9f4a2f8704ba0748444
Josscoder <josscoder@hotmail.com>
2023-03-08 17:32:19 -0500

    feat: code cleanup

--- src/main/java/josscoder/itemsid/listener/GeneralListener.java
+++ src/main/java/josscoder/itemsid/listener/GeneralListener.java
@@ -11,19 +11,19 @@ import josscoder.itemsid.ItemsIDPlugin;
 
 public class GeneralListener implements Listener {
 
-  @EventHandler
-  public void onItemHeld(PlayerItemHeldEvent event) {
-    Item item = event.getItem();
-    if (item.getId() == BlockID.AIR) {
-      return;
+    @EventHandler
+    public void onItemHeld(PlayerItemHeldEvent event) {
+        Item item = event.getItem();
+        if (item.getId() == BlockID.AIR) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+
+        if (!player.namedTag.getBoolean(ItemsIDPlugin.NBT_ITEMS_ID)) {
+            return;
+        }
+
+        player.sendActionBar(TextFormat.colorize(String.format("&9%s\n&r&7%s:%s", item.getName(), item.getId(), item.getDamage())));
     }
-
-    Player player = event.getPlayer();
-
-    if (!player.namedTag.getBoolean(ItemsIDPlugin.NBT_ITEMS_ID)) {
-      return;
-    }
-
-    player.sendActionBar(TextFormat.colorize(String.format("&9%s\n&r&7%s:%s", item.getName(), item.getId(), item.getDamage())));
-  }
 }
